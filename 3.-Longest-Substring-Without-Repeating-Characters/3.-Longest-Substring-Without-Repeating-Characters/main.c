652f853b4df10db9e2d9240199c60600a60a21c3
Jacky <h0938677616@gmail.com>
2022-07-03 13:51:12 +0800

    1. add main.c

---
+++ 3.-Longest-Substring-Without-Repeating-Characters/3.-Longest-Substring-Without-Repeating-Characters/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  3.-Longest-Substring-Without-Repeating-Characters
+//
+//  Created by 夏蜃詰 on 2022/7/3.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
